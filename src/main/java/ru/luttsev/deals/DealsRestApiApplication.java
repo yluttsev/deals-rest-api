6029c9bd93dd838fd0823085a16a3ca5451357c1
Yuri Luttsev <floomyz@vk.com>
2024-07-15 19:31:06 +0300

    init

---
+++ src/main/java/ru/luttsev/deals/DealsRestApiApplication.java
@@ -0,0 +1,13 @@
+package ru.luttsev.deals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealsRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealsRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/luttsev/deals/DealsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.luttsev.deals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealsRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
