6ae8d9e1d072d7c8e04322c1a3a845b8b8492f9a
Yuri Luttsev <floomyz@vk.com>
2024-07-24 16:49:03 +0300

    test: добавлено поле action в sql запрос

--- src/test/resources/db/outbox/insert_outbox_data.sql
+++ src/test/resources/db/outbox/insert_outbox_data.sql
@@ -1,8 +1,8 @@
 create extension if not exists pgcrypto;
 
-insert into outbox (id, contractor_id, is_main, creation_date, status)
+insert into outbox (id, contractor_id, is_main, creation_date, status, action)
 values
-    (gen_random_uuid(), 'test_id', true, now(), 'ERROR'),
-    (gen_random_uuid(), 'test_id1', false, now(), 'SUCCESS'),
-    (gen_random_uuid(), 'test_id2', true, now(), 'ERROR'),
-    (gen_random_uuid(), 'test_id2', true, now(), 'SUCCESS')
+    (gen_random_uuid(), 'test_id', true, now(), 'SUCCESS', 'CHANGE_STATUS_ACTIVE'),
+    (gen_random_uuid(), 'test_id1', false, now(), 'ERROR', 'DELETE_CONTRACTOR'),
+    (gen_random_uuid(), 'test_id2', true, now(), 'ERROR', 'CHANGE_STATUS_ACTIVE'),
+    (gen_random_uuid(), 'test_id2', false, now(), 'SUCCESS', 'CHANGE_STATUS_CLOSED')
