5642001fe773a02c9642cc2f9e8da9c830a8d9a1
Yuri Luttsev <floomyz@vk.com>
2024-08-16 17:50:31 +0300

    feat(service): добавлен поиск контрагентов в сделке по ID основного контрагента

--- src/main/java/ru/luttsev/deals/repository/DealContractorRepository.java
+++ src/main/java/ru/luttsev/deals/repository/DealContractorRepository.java
@@ -1,11 +1,17 @@
 package ru.luttsev.deals.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.luttsev.deals.model.entity.DealContractor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface DealContractorRepository extends JpaRepository<DealContractor, UUID> {
+
+    @Query("select dc from DealContractor dc where dc.contractorId = ?1")
+    List<DealContractor> findByContractorId(String contractorId);
+
 }
